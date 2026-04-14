d72f5e5c95b4944ffe630ad121d6b09633484fbb
MercyJOA <MercyJOA@users.noreply.github.com>
2026-04-15 00:38:47 +0100

    add  correct-glow and wrong-shake features to the options buttons. add images to the options.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const option1 = document.querySelector("#option-1");
 const option2 = document.querySelector("#option-2");
 const option3 = document.querySelector("#option-3");
 const option4 = document.querySelector("#option-4");
+
 const mainContainer = document.getElementById("quizContainer");
 //the quiz array with the questions and answers
 const quiz = [
@@ -43,12 +44,26 @@ let score = 0;
 let currentQuestionIndex = 0;
 
 function loggedQuiz() {
-const currentQuestionData = quiz[currentQuestionIndex]
+const currentQuestionData = quiz[currentQuestionIndex];
 question.innerText = currentQuestionData.question1;
-option1.innerHTML = `<img src="${currentQuestionData.optionA}" width="100"/>`;
+//create an array of options, elements and data keys.
+const options = [
+        {element:option1,data:currentQuestionData.optionA},
+        {element:option2,data:currentQuestionData.optionB},
+        {element:option3,data:currentQuestionData.optionC},
+        {element:option4,data:currentQuestionData.correct}];
+        options.forEach(opt =>{
+                if(typeof opt.data === "string" && opt.data.includes("asset")){
+                        opt.element.innerHTML =`<img src="${opt.data}" width="100" style="pointer-events: none;"/>`;
+                }else{
+                        opt.element.innerText = opt.data;
+                }
+        })
+
+/*option1.innerHTML = `<img src="${currentQuestionData.optionA}" width="100"/>`;
 option2.innerHTML = `<img src="${currentQuestionData.optionB}" width="100"/>`;
 option3.innerHTML = `<img src="${currentQuestionData.optionC}" width="100"/>`;
-option4.innerHTML = `<img src="${currentQuestionData.correct}" width="100"/>`;
+option4.innerHTML = `<img src="${currentQuestionData.correct}" width="100"/>`;*/
 }
 
 //start button function.
@@ -58,15 +73,22 @@ quizStartButton.addEventListener("click",()=> {
 })
 //listens to the click on the answer buttons.
 function handleAnswer(e){
-        const selectedAnswer=e.target.innerText;
+        const selectedButton= e.currentTarget;
+        const selectedAnswer=selectedButton.innerText || selectedButton.querySelector("img")?.getAttribute("src");
         const correctAnswer=quiz[currentQuestionIndex].correct;
 //check if answer is correct.
-if (selectedAnswer===correctAnswer) {
+if (selectedAnswer&&selectedAnswer.includes(correctAnswer)) {
         score++;
-        console.log("correct answer!");
+        selectedButton.classList.add("correct-glow");
 }else{
-        console.log(`wrong! The correct answer is ${correctAnswer.toUpperCase()}`);
+       selectedButton.classList.add("wrong-shake");
 }
+const allButtons = document.querySelectorAll(".answerButton");
+allButtons.forEach(btn=>btn.style.pointerEvents = "none");
+setTimeout(()=>{
+        selectedButton.classList.remove("correct-glow","wrong-shake");
+
+        allButtons.forEach(btn=>btn.style.pointerEvents = "auto");
 
 currentQuestionIndex++
 
@@ -76,18 +98,18 @@ if (currentQuestionIndex<quiz.length) {
    mainContainer.innerHTML=`<h2 style="background-color:green; color:white; padding:1rem; margin:0rem; border:none;">You Finished!</h2>
    <p style="background-color:green; color: white; padding: 1rem; margin:0rem;border:none;">Your final score is ${score} out of ${quiz.length}</p>
    <button onclick="location.reload()" style="background-color:white; color:green; padding:1rem; margin:0rem;border:none;font-size:1rem;">Restart Quiz</button>`;
-}   
+}  },1200);  
 }
 
 const options = document.querySelectorAll(".answerButton");
 for(let option of options){
         option.addEventListener("click",handleAnswer);
 }
-quizNextButton.addEventListener("click",handleAnswer)
- currentQuestionIndex++;
+
+  
 
   quizPreviousButton.addEventListener("click",()=>{
-        if(currentQuestionIndex >= 0){
+        if(currentQuestionIndex > 0){
                 currentQuestionIndex--;
                 loggedQuiz();
                 
